c2c6a937c5c340577467a1d23d7c25e09bdb8225
erin johnson <erin.nicole.johnson@gmail.com>
2018-12-19 18:48:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ theBeatlesPlay(musicians, instruments);
 
 function johnLennonFacts(fact) {
   const facts = [
-      "He was the last Beatle to learn to drive",
+    /*  "He was the last Beatle to learn to drive",
       "He was never a vegetarian",
       "He was a choir boy and boy scout",
-      "He hated the sound of his own voice"
+      "He hated the sound of his own voice" */
     ];
   var newFacts = [];
   let x = 0;
